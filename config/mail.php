e8fe8476cb23bbfbd76d7cd2b646671f1ba81157
yashensha-dms <yashensha@dms.family>
2026-07-28 18:26:13 +0530

    feat: initialize mail configuration file for backend email services

--- config/mail.php
+++ config/mail.php
@@ -37,7 +37,7 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'port' => is_numeric(env('MAIL_PORT')) ? (int) env('MAIL_PORT') : 587,
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
